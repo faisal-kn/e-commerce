039835a5d82af02d927f9756f3d9adcec27b91f5
aritro66 <ghosharitro66@gmail.com>
2021-12-16 11:09:30 +0530

    appling redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import React,{useEffect} from "react";
+import {useDispatch,useSelector} from "react-redux";
+import { showAll } from "./redux/action/actionlist";
 const App = () => {
-  return <div>Hello World</div>;
+  const dispatch=useDispatch();
+  useEffect(() => {
+    dispatch(showAll());
+  }, [])
+  const data=useSelector(state=>state.DataAll);
+  console.log(data);
+  function handle() {
+    console.log(data);
+  }
+  return <div>Hello World
+    {/* <button onClick={()=>{dispatch(showAll())}}>+</button> */}
+    <button onClick={handle}>data</button>
+  </div>;
 };
 
 export default App;
